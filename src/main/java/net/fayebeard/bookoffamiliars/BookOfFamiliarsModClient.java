22fa6f8ff52fcd3db1541b26f2b2c77f36008dbf
Fayebeard <fayehob56@gmail.com>
2026-05-05 16:55:35 +0200

    Added in-game config menu

---
+++ src/main/java/net/fayebeard/bookoffamiliars/BookOfFamiliarsModClient.java
@@ -0,0 +1,24 @@
+package net.fayebeard.bookoffamiliars;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = BookOfFamiliarsMod.MOD_ID, dist = Dist.CLIENT)
+
+@EventBusSubscriber(modid = BookOfFamiliarsMod.MOD_ID, value = Dist.CLIENT)
+public class BookOfFamiliarsModClient {
+    public BookOfFamiliarsModClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
--- src/main/java/net/fayebeard/bookoffamiliars/Config.java
+++ src/main/java/net/fayebeard/bookoffamiliars/Config.java
@@ -11,11 +11,13 @@ public class Config {
     public static final ModConfigSpec.IntValue MAX_FAMILIARS = BUILDER
             .comment("Maximum number of familiars a player can store.",
                     "WARNING: Higher values may cause performance issues, especially on servers.")
+            .translation("bookoffamiliars.configuration.maxFamiliars")
             .defineInRange("maxfamiliars", 10, 1, 100);
 
     public static final ModConfigSpec.ConfigValue<List<? extends String>> ENTITY_BLACKLIST = BUILDER
             .comment("List of entity types that cannot be stored. Format: ['minecraft:wolf', 'minecraft:cat']",
                     "Tip: blacklist horses if you don't want players stealing other people's.")
+            .translation("bookoffamiliars.configuration.entityBlacklist")
             .defineListAllowEmpty("entityBlackList", List.of(), entry -> entry instanceof String);
 
     static final ModConfigSpec SPEC = BUILDER.build();
