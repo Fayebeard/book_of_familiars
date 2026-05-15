c882703e1a424008ec74b61156f5d3674a0699d8
Fayebeard <fayehob56@gmail.com>
2026-05-15 19:27:15 +0200

    Added default support for storing the Fox, Iron Golem, Snow Golem, and Strider

--- src/main/java/net/fayebeard/bookoffamiliars/item/custom/FamiliarBookItem.java
+++ src/main/java/net/fayebeard/bookoffamiliars/item/custom/FamiliarBookItem.java
@@ -8,6 +8,9 @@ import net.fayebeard.bookoffamiliars.network.OpenFamiliarBookPacket;
 import net.fayebeard.bookoffamiliars.sounds.ModSounds;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -19,8 +22,12 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.animal.Fox;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.animal.SnowGolem;
 import net.minecraft.world.entity.animal.allay.Allay;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.monster.Strider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -36,6 +43,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class FamiliarBookItem extends Item {
+
+    private static final String OWNER_TAG = "bookoffamiliars:owner";
+
     public FamiliarBookItem(Properties properties) {
         super(properties);
     }
@@ -103,12 +113,38 @@ public class FamiliarBookItem extends Item {
                     ? allay.getCustomName().getString()
                     : allay.getType().getDescription().getString();
 
-        } else if (Config.ENTITY_WHITELIST.get().contains(entityId)) {
+        } else if (Config.ENTITY_WHITELIST.get().contains(entityId)
+                    || entity instanceof SnowGolem
+                    || entity instanceof IronGolem
+                    || entity instanceof Strider) {
             entity.save(nbt);
+            if (!checkAndSetOwnership(player, nbt)) return false;
             entityType = entity.getType().getDescriptionId();
             displayName = entity.hasCustomName() && entity.getCustomName() != null
                     ? entity.getCustomName().getString()
                     : entity.getType().getDescription().getString();
+
+        } else if (entity instanceof Fox fox) {
+            fox.save(nbt);
+            boolean trustsPlayer = false;
+            ListTag trustedList = nbt.getList("Trusted", Tag.TAG_INT_ARRAY);
+            for (Tag tag : trustedList) {
+                UUID uuid = NbtUtils.loadUUID(tag);
+                if (uuid.equals(player.getUUID())) {
+                    trustsPlayer = true;
+                    break;
+                }
+            }
+
+            if (!trustsPlayer) {
+                player.sendSystemMessage(Component.translatable("bookoffamiliars.not_your_familiar"));
+                return false;
+            }
+
+            entityType = fox.getType().getDescriptionId();
+            displayName = fox.hasCustomName() && fox.getCustomName() != null
+                    ? fox.getCustomName().getString()
+                    : fox.getType().getDescription().getString();
         } else {
             return false;
         }
@@ -173,5 +209,18 @@ public class FamiliarBookItem extends Item {
         );
 
         super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+    }
+
+    private boolean checkAndSetOwnership(Player player, CompoundTag nbt) {
+        if (nbt.contains(OWNER_TAG)) {
+            UUID ownerUUID = nbt.getUUID(OWNER_TAG);
+            if (!ownerUUID.equals(player.getUUID())) {
+                player.sendSystemMessage(Component.translatable("bookoffamiliars.not_your_familiar"));
+                return false;
+            }
+        } else {
+            nbt.putUUID(OWNER_TAG, player.getUUID());
+        }
+        return true;
     }
 }
